f5bec6733534345dce8d24418d7b0e6ec7553d63
David Leske <leske.david@gmail.com>
2022-03-23 17:58:24 -0500

    update

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -79,12 +79,23 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/DALeske/Mapping_Earthquakes/main/torontoRoutes.json";
 
+console.group(torontoData);
 
-// Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data
 d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+      color: "#ffffa1",
+      weight:2,
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
+          + feature.properties.dst + "</h3>");
+      }
+
+
+  })
+  .addTo(map);
 });
 
 
